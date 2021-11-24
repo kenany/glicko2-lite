1da099b1f530f4f1f8e4bc77afaf154296d45ee5
Kenan Yildirim <kenan@kenany.me>
2021-11-23 19:27:19 -0500

    fix: add type coverage through JSDoc

--- index.js
+++ index.js
@@ -4,20 +4,52 @@ var assign = require('lodash.assign');
 var map = require('lodash.map');
 var reduce = require('lodash.reduce');
 
+/** @typedef {[rating: number, rd: number, vol: number]} Opponent */
+
+/**
+ * @typedef {object} ScaledOpponent
+ * @property {number} muj
+ * @property {number} phij
+ * @property {number} gphij
+ * @property {number} emmp
+ * @property {number} score
+*/
+
+/**
+ * @param {number} rating
+ * @param {number} rd
+ * @param {{ rating: number; }} options
+ */
 function scale(rating, rd, options) {
   var mu = (rating - options.rating) / 173.7178;
   var phi = rd / 173.7178;
   return { mu: mu, phi: phi };
 }
 
+/**
+ * @param {number} phi
+ * @returns {number}
+ */
 function g(phi) {
   return 1 / Math.sqrt(1 + 3 * Math.pow(phi, 2) / Math.pow(Math.PI, 2));
 }
 
+/**
+ * @param {number} mu
+ * @param {number} muj
+ * @param {number} phij
+ * @returns {number}
+ */
 function e(mu, muj, phij) {
   return 1 / (1 + Math.exp(-g(phij) * (mu - muj)));
 }
 
+/**
+ * @param {number} mu
+ * @param {readonly Opponent[]} opponents
+ * @param {{ rating: number; }} options
+ * @returns {ScaledOpponent[]}
+ */
 function scaleOpponents(mu, opponents, options) {
   return map(opponents, function(opp) {
     var scaled = scale(opp[0], opp[1], options);
@@ -31,21 +63,41 @@ function scaleOpponents(mu, opponents, options) {
   });
 }
 
+/**
+ * @param {readonly ScaledOpponent[]} opponents
+ * @returns {number}
+ */
 function updateRating(opponents) {
   return 1 / reduce(opponents, function(sum, opp) {
     return sum + Math.pow(opp.gphij, 2) * opp.emmp * (1 - opp.emmp);
   }, 0);
 }
 
+/**
+ * @param {number} v
+ * @param {readonly ScaledOpponent[]} opponents
+ * @returns {number}
+ */
 function computeDelta(v, opponents) {
   return v * reduce(opponents, function(sum, opp) {
     return sum + opp.gphij * (opp.score - opp.emmp);
   }, 0);
 }
 
+/**
+ * @param {number} phi
+ * @param {number} v
+ * @param {number} delta
+ * @param {number} a
+ * @param {{ tau: number; }} options
+ */
 function volF(phi, v, delta, a, options) {
   var phi2 = Math.pow(phi, 2);
   var d2 = Math.pow(delta, 2);
+
+  /**
+   * @param {number} x
+   */
   return function(x) {
     var ex = Math.exp(x);
     var a2 = phi2 + v + ex;
@@ -55,12 +107,21 @@ function volF(phi, v, delta, a, options) {
   };
 }
 
+/**
+ * @param {number} sigma
+ * @param {number} phi
+ * @param {number} v
+ * @param {number} delta
+ * @param {{ tau: number; }} options
+ * @returns {number}
+ */
 function computeVolatility(sigma, phi, v, delta, options) {
   // 5.1
   var a = Math.log(Math.pow(sigma, 2));
   var f = volF(phi, v, delta, a, options);
 
   // 5.2
+  /** @type {number} */
   var b;
   if (Math.pow(delta, 2) > Math.pow(phi, 2) + v) {
     b = Math.log(Math.pow(delta, 2) - Math.pow(phi, 2) - v);
@@ -79,6 +140,7 @@ function computeVolatility(sigma, phi, v, delta, options) {
 
   // 5.4
   while (Math.abs(b - a) > 0.000001) {
+    /** @type {number} */
     var c = a + (a - b) * fa / (fb - fa);
     var fc = f(c);
 
@@ -98,10 +160,21 @@ function computeVolatility(sigma, phi, v, delta, options) {
   return Math.exp(a / 2);
 }
 
+/**
+ * @param {number} sigmap
+ * @param {number} phi
+ * @returns {number}
+ */
 function phiStar(sigmap, phi) {
   return Math.sqrt(Math.pow(sigmap, 2) + Math.pow(phi, 2));
 }
 
+/**
+ * @param {number} phis
+ * @param {number} mu
+ * @param {number} v
+ * @param {readonly ScaledOpponent[]} opponents
+ */
 function newRating(phis, mu, v, opponents) {
   var phip = 1 / Math.sqrt(1 / Math.pow(phis, 2) + 1 / v);
   var mup = mu + Math.pow(phip, 2) * reduce(opponents, function(sum, opp) {
@@ -110,18 +183,32 @@ function newRating(phis, mu, v, opponents) {
   return { mu: mup, phi: phip };
 }
 
+/**
+ * @param {number} mup
+ * @param {number} phip
+ * @param {{ rating: number; }} options
+ */
 function unscale(mup, phip, options) {
   var rating = 173.7178 * mup + options.rating;
   var rd = 173.7178 * phip;
   return { rating: rating, rd: rd };
 }
 
+/**
+ * @param {number} rating
+ * @param {number} rd
+ * @param {number} sigma
+ * @param {readonly Opponent[]} opponents
+ * @param {{ rating?: number; tau?: number; }} [options]
+ * @returns {{ rating: number; rd: number; vol: number; }}
+ */
 function rate(rating, rd, sigma, opponents, options) {
-  options = assign({}, { rating: 1500, tau: 0.5 }, options || {});
+  /** @type {{ rating: number; tau: number; }} */
+  var opts = assign({}, { rating: 1500, tau: 0.5 }, options || {});
 
   // Step 2
-  var scaled = scale(rating, rd, options);
-  var scaledOpponents = scaleOpponents(scaled.mu, opponents, options);
+  var scaled = scale(rating, rd, opts);
+  var scaledOpponents = scaleOpponents(scaled.mu, opponents, opts);
 
   // Step 3
   var v = updateRating(scaledOpponents);
@@ -130,7 +217,7 @@ function rate(rating, rd, sigma, opponents, options) {
   var delta = computeDelta(v, scaledOpponents);
 
   // Step 5
-  var sigmap = computeVolatility(sigma, scaled.phi, v, delta, options);
+  var sigmap = computeVolatility(sigma, scaled.phi, v, delta, opts);
 
   // Step 6
   var phis = phiStar(sigmap, scaled.phi);
@@ -138,9 +225,7 @@ function rate(rating, rd, sigma, opponents, options) {
   // Step 7
   var updated = newRating(phis, scaled.mu, v, scaledOpponents);
 
-  var unscaled = unscale(updated.mu, updated.phi, options);
-  unscaled.vol = sigmap;
-  return unscaled;
+  return assign({}, unscale(updated.mu, updated.phi, opts), { vol: sigmap });
 }
 
 module.exports = rate;
